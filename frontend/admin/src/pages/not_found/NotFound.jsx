8ef560ebcc8f99d07689af7695c01f21e2464297
admin_signin <siemtra78@gmail.com>
2025-05-29 15:10:34 +0700

    push

--- frontend/admin/src/pages/not_found/NotFound.jsx
+++ frontend/admin/src/pages/not_found/NotFound.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
-import "./NotFound.css";
+import "./Notfound.css";
 
 function NotFoundPage() {
   const navigate = useNavigate();
