779291ad588e5648eb5cebb6e3bd2cc51143d1b8
admin_signin <siemtra78@gmail.com>
2025-06-05 07:58:29 +0700

    push

--- frontend/admin/src/pages/auth/Login.jsx
+++ frontend/admin/src/pages/auth/Login.jsx
@@ -17,13 +17,6 @@ const Login = () => {
   return (
     <Spin spinning={loading}>
       <div className="login-container">
-        <video autoPlay loop muted playsInline className="video-background">
-          <source
-            src="../../logo/istockphoto-1752533608-640_adpp_is.mp4"
-            type="video/mp4"
-          />
-          Your browser does not support HTML5 video.
-        </video>
 
         <div className="video-overlay"></div>
 
