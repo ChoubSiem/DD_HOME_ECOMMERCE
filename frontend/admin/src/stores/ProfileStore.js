f4fc39fdb6efa0fd9e671930d91a06831cf95cb6
siem <siemtra78@gmail.com>
2025-06-09 13:25:19 +0700

    push

--- frontend/admin/src/stores/ProfileStore.js
+++ frontend/admin/src/stores/ProfileStore.js
@@ -44,16 +44,16 @@ export const useProfileStore = create((set) => ({
         Cookies.remove("permission");
       }
     } catch (error) {
-      set({
-        isAuthenticated: false,
-        token,
-        user: null,
-        permissions:null,
-        loading: false,
-      });
-      localStorage.removeItem('token');
-      Cookies.remove('user');
-      navigate("/login");
+      // set({
+      //   isAuthenticated: false,
+      //   token,
+      //   user: null,
+      //   permissions:null,
+      //   loading: false,
+      // });
+      // localStorage.removeItem('token');
+      // Cookies.remove('user');
+      // navigate("/login");
     }
   },
   
