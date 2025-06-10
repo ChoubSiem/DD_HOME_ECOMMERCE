a78fe64c971a0413744e6ae0f6f995efefcae8a5
siem <siemtra78@gmail.com>
2025-06-10 15:58:26 +0700

    push

--- frontend/admin/vite.config.js
+++ frontend/admin/vite.config.js
@@ -6,5 +6,14 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_SIOUKS_API, // Make sure the env variable name matches what you're using
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '') // Optional: removes /api prefix when forwarding
+      }
+    }
   }
 })
