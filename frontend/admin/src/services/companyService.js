055ebc34ee73013b04aced24d9743b646a7a556f
admin_signin <siemtra78@gmail.com>
2025-07-05 13:32:15 +0700

    push

--- frontend/admin/src/services/companyService.js
+++ frontend/admin/src/services/companyService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://127.0.0.1:8000/api';
+const API_BASE = 'https://backend.ddhomekh.com/api';
 
 export const fetchCompany = async (token) => {
   const response = await axios.get(`${API_BASE}/company/list`, {
